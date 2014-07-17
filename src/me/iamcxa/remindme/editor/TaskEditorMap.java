54e0c00abcb968adf1ab8bd0c8f82e2f964aed30
FuYaoDe <dan826252005@yahoo.com.tw>
2014-07-17 18:25:17 +0800

    新的地圖

--- src/me/iamcxa/remindme/editor/TaskEditorMap.java
+++ src/me/iamcxa/remindme/editor/TaskEditorMap.java
@@ -1,16 +1,18 @@
 package me.iamcxa.remindme.editor;
 
-import me.iamcxa.remindme.R;
+
 import android.app.Fragment;
 import android.os.Bundle;
-import android.view.View;
+
 
 public class TaskEditorMap extends Fragment {
 
+	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
+		
 	}
 
 	@Override
--- src/me/iamcxa/remindme/editor/TaskEditorTab.java
+++ src/me/iamcxa/remindme/editor/TaskEditorTab.java
@@ -5,16 +5,19 @@ import android.R.string;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class TaskEditorTab extends Activity {
+public class TaskEditorTab extends FragmentActivity  {
 
 
 	private static EditorVar mEditorVar ;
+	public static FragmentManager fragmentManager;
 
 
 	/** Called when the activity is first created. */
@@ -22,7 +25,7 @@ public class TaskEditorTab extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_task_editor_tab);
-
+		fragmentManager=getFragmentManager();
 		setupViewComponent();
 	}
 	// This is the action bar menu
